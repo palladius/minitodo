5eafd5381f21f4b564b9ec3b39af68370cbbc502
Riccardo <rusko@palladius.it>
2010-09-16 18:11:42 +0100

    first add Rails 3 project, yay!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minitodo::Application.config.secret_token = 'bc552c4d3631c04b61e7586e3ceffa453248174043547f5449445569718219bc31d06d9aea23aff7771e4cac512cf8ac03de930ccd9f92cde07d2942c69d55c1'
